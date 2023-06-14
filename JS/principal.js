f237c5b1a9f2584484c16c13a4343868836cbc7a
Edgar Emmanuel <edgarvelezmendez@hotmail.com>
2023-06-13 21:18:57 -0600

    Inicio del Proyecto :P

---
+++ JS/principal.js
@@ -0,0 +1,20 @@
+var imagenes = [
+    "/Img/imagen.jpg",
+    "/Img/imagen1.jpg",
+    "/Img/imagen2.jpg"
+    ];
+    var index = 0;
+  var tiempoIntervalo = 5000; // Cambiar imagen cada 5 segundos
+    
+    function cambiarImagen() {
+    document.getElementById("imagen").src = imagenes[index];
+    index++;
+    if (index === imagenes.length) {
+        index = 0;
+    }
+}
+    setInterval(cambiarImagen, tiempoIntervalo);
+
+    
+      
+    
