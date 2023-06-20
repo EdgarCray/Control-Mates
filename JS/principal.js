c38951fd7ddcd318961bb01feb1b8dcbaf5a472d
Edgar Emmanuel <edgarvelezmendez@hotmail.com>
2023-06-20 04:04:00 -0600

    Finalización del menú

--- JS/principal.js
+++ JS/principal.js
@@ -1,20 +1,3 @@
-var imagenes = [
-    "/Img/imagen.jpg",
-    "/Img/imagen1.jpg",
-    "/Img/imagen2.jpg"
-    ];
-    var index = 0;
-  var tiempoIntervalo = 5000; // Cambiar imagen cada 5 segundos
-    
-    function cambiarImagen() {
-    document.getElementById("imagen").src = imagenes[index];
-    index++;
-    if (index === imagenes.length) {
-        index = 0;
-    }
-}
-    setInterval(cambiarImagen, tiempoIntervalo);
-
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const  navMenu = document.getElementById('nav-menu'), 
 navToggle = document.getElementById('nav-toggle'), 
